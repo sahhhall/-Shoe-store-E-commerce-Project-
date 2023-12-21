cd55e24adcab7724896d3505ee3a3589d2cd0a80
sahhhall <sahalvavoor313@gmail.com>
2023-12-21 12:02:18 +0530

    cartcontroller updated added items

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,37 +1,89 @@
 const User = require('../models/userModel');
-const  bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const Category = require('../models/categoriesModel');
 const Product = require('../models/productSchema')
+const Cart = require('../models/cartSchema')
 
 
-
-const loadCart = async (req,res)=>{
-    try{
+//=============================================LOAD CART PAGE=========================================================//
+const loadCart = async (req, res) => {
+    try {
         res.render('cartPage');
-    }catch(err){
+    } catch (err) {
         console.log(err.message);
     }
 }
-const addtoCart = async(req,res)=>{
-    try{
 
+//=============================================PRODUCT ADDING TO CART==================================================//
+const addtoCart = async (req, res) => {
+    try { // console.log(productData+"quantuty is"+productquantity);
+        if (!req.session.user || !req.session.user._id) {
+            return res.json({login: true, message: "Please login and continue shopping!"});
+        } else {
             const userId = req.session.user._id;
-          
-            if (userId === undefined) {
-                res.json({ login: true, message: "Please login and continue shopping!" });
-                res.redirect('/signin');
-              }
-           
+            const userData = await User.findOne({_id: userId});
+            const {productId, productquantity} = req.body;
+            const productData = await Product.findOne({_id: productId});
+            const cart = await Cart.findOne({userid: userId});
+            const productprice = productData.price;
+            // console.log(productprice)
+            if (cart) {
+                    // we need to check the product exist or not because if exist we need onlu updation 
 
-    }catch(err){
+                const existProduct = cart.products.find((pro) => pro.productId.toString() == productId);
+                if (existProduct) {
+                    await Cart.findOneAndUpdate({
+                        userid: userId,
+                        "products.productId": productId
+                    }, {
+                        $inc: {
+                            "products.$.quantity": productquantity,
+                            "products.$.totalPrice": productquantity * existProduct.productPrice
+                        }
+                    })
+                } else {
+                //    add new product to cart 
+                await Cart.findOneAndUpdate({userid:userId   },
+                    {
+                        $push:{
+                            products:{
+                                productId: productId,
+                                quantity: productquantity,
+                                productPrice: productData.price,
+                                totalPrice: productquantity *productData.price
+                            }
+                        }
+                    }
+                    
+                    )
+                }
+            } 
+            
+            
+            else { // create bew cart  here am adding new product to cart
+                const newCart = new Cart({
+                    userid: userId,
+                    products: [
+                        {
+                            productId: productId,
+                            quantity: productquantity,
+                            productPrice: productData.price,
+                            totalPrice: productquantity *productData.price
+                        }
+                    ]
+                })
+                await newCart.save();
+            }
+            res.json({success: true});
+        }
+
+    } catch (err) {
         console.log(err.message)
     }
 }
 
 
-
-
-module.exports ={
+module.exports = {
     loadCart,
     addtoCart
 }
